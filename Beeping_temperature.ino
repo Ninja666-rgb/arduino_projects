4b291a6887fc26b347c46ebe2fb70c49e7f50a1a
Ninja666-rgb <80719864+Ninja666-rgb@users.noreply.github.com>
2023-04-15 09:33:21 +1200

    Create Beeping_temperature.ino

---
+++ Beeping_temperature.ino
@@ -0,0 +1,22 @@
+float R1 = 10000;
+
+void setup(){
+  Serial.begin(115200);
+  delay(500);
+  pinMode(8, OUTPUT);
+}
+
+void loop(){
+  int Vo = analogRead(A0);
+  float R2 = R1 / (1023.0 / (float)Vo - 1.0);
+  float logR2 = log(R2);
+  float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+  float T = (1.0 / (c1 + c2*logR2 + c3*logR2*logR2*logR2));
+  float tempC = T - 273.15;
+  if(tempC > 20){
+    tone(8, 800);
+    delay(200);
+    noTone(8);
+    delay(200);
+  }
+}

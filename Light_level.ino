2afa4be1b5ec779588a7f08cff3b95533424f5d3
Ninja666-rgb <80719864+Ninja666-rgb@users.noreply.github.com>
2023-04-14 18:54:48 +1200

    Create Light_level.ino

---
+++ Light_level.ino
@@ -0,0 +1,30 @@
+void setup(){
+  pinMode(2, INPUT);
+  for(int i = 3; i < 14; i++){
+    pinMode(i, OUTPUT);
+  }
+}
+
+unsigned long last_pressed;
+int l_p, level;
+
+void loop(){
+  int p = digitalRead(5);
+  if(l_p == HIGH && p == LOW){
+    if(level < 11){
+      level++;
+    }
+    last_pressed = millis();
+  }
+  if(last_pressed+500 < millis()){
+    if(level > 0){
+      level--;
+    }
+  }
+  for(int i = 3; i < level+3; i++){
+    digitalWrite(i, HIGH);
+  }
+  for(int i = level+3; i < 14; i++){
+    digitalWrite(i, LOW);
+  }
+}

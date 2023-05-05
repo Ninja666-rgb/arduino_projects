e1f25c1d80321ef3cb147f32a2885eff71b719fb
Ninja666-rgb <80719864+Ninja666-rgb@users.noreply.github.com>
2023-05-06 09:39:23 +1200

    Update Light_level.ino

--- Light_level.ino
+++ Light_level.ino
@@ -1,5 +1,6 @@
 void setup(){
-  pinMode(5, INPUT);
+  Serial.begin(115200);
+  pinMode(2, INPUT);
   for(int i = 3; i < 14; i++){
     pinMode(i, OUTPUT);
   }
@@ -9,16 +10,17 @@ unsigned long last_pressed;
 int l_p, level;
 
 void loop(){
-  int p = digitalRead(5);
+  int p = digitalRead(2);
   if(l_p == HIGH && p == LOW){
     if(level < 11){
       level++;
     }
     last_pressed = millis();
   }
-  if(last_pressed+500 < millis()){
+  if(last_pressed+(13-level)*40 < millis()){
     if(level > 0){
       level--;
+      last_pressed = millis();
     }
   }
   for(int i = 3; i < level+3; i++){
@@ -27,4 +29,5 @@ void loop(){
   for(int i = level+3; i < 14; i++){
     digitalWrite(i, LOW);
   }
+  l_p = p;
 }

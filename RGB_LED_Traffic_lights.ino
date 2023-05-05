1171162e7c00cb7b8367040e8a29a1305e422a7a
Ninja666-rgb <80719864+Ninja666-rgb@users.noreply.github.com>
2023-05-06 10:03:08 +1200

    Update RGB_LED_Traffic_lights.ino

--- RGB_LED_Traffic_lights.ino
+++ RGB_LED_Traffic_lights.ino
@@ -18,11 +18,15 @@ void RGB(int r, int g, int b){
 void loop(){
   RGB(0, 255, 0);//green
   if(digitalRead(5) == LOW){
-    delay(5000);
     RGB(255, 255, 0);//yellow
     delay(2000);
     RGB(255, 0, 0);//red
-    delay(10000);
+    while(digitalRead(5) == LOW){
+    }
+    while(digitalRead(5) == HIGH){
+    }
     RGB(0, 255, 0);//green
+    while(digitalRead(5) == LOW){
+    }
   }
 }

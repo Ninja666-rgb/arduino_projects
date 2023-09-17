10d82c12a95234a89a2ea6bc12cf579fa95f2d83
Ninja666-rgb <80719864+Ninja666-rgb@users.noreply.github.com>
2023-09-17 17:54:10 +1200

    Update Arduino.ino

--- Robot_Controller/Arduino/Arduino.ino
+++ Robot_Controller/Arduino/Arduino.ino
@@ -6,14 +6,28 @@
 
 #define echoPin 13
 #define trigPin 12
+#define firewall 8
+
+int lasts[firewall];
 
 int motor_speed_LOW = 85;
 int motor_speed_MID = 100;
 int motor_speed_HIGH = 140;
 
+unsigned long cool;
+
 Robot robot;
 
 SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+
+int aver(){
+  int s = 0;
+  for(int i = 0; i < firewall; i++){
+    s += lasts[i];
+  }
+  s = s/firewall;
+  return s;
+}
 
 int getUltraDistance(){
   digitalWrite(trigPin, LOW);
@@ -22,11 +36,22 @@ int getUltraDistance(){
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
   long duration = pulseIn(echoPin, HIGH, 6000);
-  int distance = (duration * 0.01715);
+  int distance = (duration*0.01715);
   if(distance == 0){
-    distance = 1000000;
+    distance = 100;
   }
-  return distance;
+  if(abs(aver()-distance) < 20){
+    for(int i = 0; i < firewall; i++){
+      lasts[i] = lasts[i+1];
+    }
+    lasts[firewall-1] = distance;
+    return distance;
+  }
+  for(int i = 0; i < firewall; i++){
+    lasts[i] = lasts[i+1];
+  }
+  lasts[firewall-1] = distance;
+  return getUltraDistance();
 }
 
 void left(int speed){
@@ -65,19 +90,22 @@ void forwun(int speed, int distan){
   int dist = getUltraDistance();
   while(dist > distan){
     dist = getUltraDistance();
+    if(millis() > cool){
+      mySerial.println("INFO"+String(dist));
+      cool = millis()+100;
+    }
   }
   stahp();
 }
 
-void backun(int speed, int distan){
-  speed = map(speed, 0, 10, motor_speed_LOW, motor_speed_HIGH);
-  robot.left_motor(-speed);
-  robot.right_motor(-speed);
-  int dist = getUltraDistance();
-  while(dist > distan){
-    dist = getUltraDistance();
+void delay2(unsigned long mil){
+  mil = mil+millis();
+  while(mil > millis()){
+    if(millis() > cool){
+      mySerial.println("INFO"+String(getUltraDistance()));
+      cool = millis()+100;
+    }
   }
-  stahp();
 }
 
 void setup() {
@@ -92,6 +120,10 @@ void setup() {
 }
 
 void loop() {
+  if(millis() > cool){
+    mySerial.println("INFO"+String(getUltraDistance()));
+    cool = millis()+100;
+  }
   if(mySerial.available()){
     String data = mySerial.readStringUntil('\n');
     mySerial.println("Running");
@@ -111,21 +143,19 @@ void loop() {
       }
       if(data.charAt(i) == 'f'){
         forw(atoi(str1.c_str()));
-        delay(atof(str2.c_str())*1000);
+        delay2(atof(str2.c_str())*1000);
       }else if(data.charAt(i) == 'b'){
         back(atoi(str1.c_str()));
-        delay(atof(str2.c_str())*1000);
+        delay2(atof(str2.c_str())*1000);
       }else if(data.charAt(i) == 'l'){
         left(atoi(str1.c_str()));
-        delay(atof(str2.c_str())*1000);
+        delay2(atof(str2.c_str())*1000);
       }else if(data.charAt(i) == 'r'){
         right(atoi(str1.c_str()));
-        delay(atof(str2.c_str())*1000);
-      }/*else if(data.charAt(i) == 'F'){
+        delay2(atof(str2.c_str())*1000);
+      }else if(data.charAt(i) == 'F'){
         forwun(atoi(str1.c_str()), atof(str2.c_str()));
-      }else if(data.charAt(i) == 'B'){
-        backun(atoi(str1.c_str()), atof(str2.c_str()));
-      }*/
+      }
       stahp();
       i = i2+1;
     }
